94eeb8b7c0b396a3588659c8369b3a3be62a6d45
NikoHamster <40576160+SurmaAa@users.noreply.github.com>
2019-05-17 20:40:11 +0200

    sprint, for these who can't use template strings

---
+++ sprint.js
@@ -0,0 +1,35 @@
+const sprint = (function () {
+    const variableStamp = "%v";
+    const escapeCharacter = "%";
+    const escapeStamp = escapeCharacter + variableStamp;
+    const regexOptions = "g"
+    const regex = new RegExp(variableStamp + "|" + escapeStamp, regexOptions);
+
+    const sprintValidate = function (replaceCount, variablesCount) {
+        if (variablesCount > replaceCount) {
+            console.warn("Too many arguments.");
+        } else if (variablesCount < replaceCount) {
+            console.warn("Too few arguments.");
+        }
+    }
+
+    const execute = function (stringDeclaration, ...variablesPassed) {
+        const variablesCount = variablesPassed.length;
+        const arrOfVariables = variablesPassed;
+        let string = stringDeclaration;
+
+        let i = 0;
+        string = string.replace(regex, function (character) {
+            if (character === escapeStamp) {
+                return variableStamp;
+            } else {
+                return arrOfVariables[i++] || variableStamp;
+            }
+        });
+
+        sprintValidate(i, variablesCount);
+        return string;
+    }
+
+    return execute;
+})();
